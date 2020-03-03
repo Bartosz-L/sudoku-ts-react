c0e69f33e6dd0a3fe4b0435189e379a01214ad60
Bartosz Ligęza <ligeza.bartosz@gmail.com>
2020-03-03 09:23:59 +0100

    display button numbers in app

--- src/components/numbers/index.tsx
+++ src/components/numbers/index.tsx
@@ -1,13 +1,13 @@
 import React, { FC } from 'react'
 import { NUMBERS } from 'typings'
 import { Container } from './styles'
+import Button from './button'
 
 const Numbers: FC = () => {
   return (
     <Container>
-      Numbers
       {([1, 2, 3, 4, 5, 6, 7, 8, 9] as NUMBERS[]).map(value => (
-        <button key={value}>{value}</button>
+        <Button key={value} value={value} />
       ))}
     </Container>
   )
